57764f8bf42c59314ebfbcf78d54a3a2d5a2bf13
kakacarlos2324 <carlosabreu20340@gmail.com>
2023-08-09 23:57:39 -0300

    trabalho01

---
+++ trabalho01.py
@@ -0,0 +1,20 @@
+# nome, sobrenome, idade, altura, peso, maior_de_idade #
+
+nome = "Carlos"
+
+sobrenome = "Gabriel"
+
+idade = 18
+
+altura = 1.72
+
+peso = 75
+
+maior_de_idade = idade >= 18
+
+print("nome", nome)
+print("sobrenome", sobrenome)
+print("idade", idade, "anos")
+print("altura", altura, "metros")
+print("peso", peso, "kg")
+print("maior_de_idade", "sim" if maior_de_idade else "não")
